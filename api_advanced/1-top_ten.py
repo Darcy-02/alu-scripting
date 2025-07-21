d30d6ee446f8a97783d2a182863b4fc4f84bea80
Mbanza Teta Darcy <d.mbanza@alustudenty.com>
2025-07-21 12:52:09 -0700

    completed from 0 to 3

---
+++ api_advanced/1-top_ten.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" Get the titles of the first 10 hot posts from a subreddit."""
+import requests
+
+
+def top_ten(subreddit):
+    headers = {'User-Agent': 'CassieBot/1.0'}
+    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
+    response = requests.get(url, headers=headers, allow_redirects=True)
+    print("Status: {}".format(response.status_code))
+
+    if response.status_code == 200:
+        json_data = response.json()
+        posts = json_data.get('data', {}).get('children', [])
+        for post in posts[:10]:
+            print(post.get('data', {}).get('title'))
+    else:
+        print(None)
